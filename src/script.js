4773f92594ed9825d3c8452ee5988a0afb4ee410
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 12:41:33 +0530

    using unshift

--- src/script.js
+++ src/script.js
@@ -170,3 +170,9 @@ const myArray = [["John", 23], ["dog", 3]];
 
 // Only change code below this line
 const removedFromMyArray = myArray.shift();
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
