faa73ba4d74906a21bad5b8b5610839186af9899
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:50:51 +0530

    Using object for lookups

--- src/script.js
+++ src/script.js
@@ -683,5 +683,26 @@ const myDog = {
   // Only change code below this line
   
   delete myDog.tails 
+  // Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    const d = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank",
+    }
+  
+    result = d[val]
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
 
   
