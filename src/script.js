7737622b296ba59108f9fc112da0df438fc602f0
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:40:55 +0530

    access objects wit bracket

--- src/script.js
+++ src/script.js
@@ -640,3 +640,11 @@ const testObj = {
   
   const hatValue = testObj.hat;      // Change this line
   const shirtValue = testObj.shirt;    // Change this line 
+  // Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line 
