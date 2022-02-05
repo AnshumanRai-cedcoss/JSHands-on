4916b9341544bfb602fed3ebf59e3f821090e9b5
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 12:56:18 +0530

    global variable

--- src/script.js
+++ src/script.js
@@ -192,3 +192,23 @@ function reusableFunction(){
       timesFive(5);
       timesFive(2);
       timesFive(0);
+      // Declare the myGlobal variable below this line
+
+const myGlobal=10 ;
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
