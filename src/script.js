03d3246e224b822f19e30b8d3df58367c8c71325
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 12:36:20 +0530

    using shift

--- src/script.js
+++ src/script.js
@@ -165,3 +165,8 @@ const myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line
  const removedFromMyArray=myArray.pop();
 console.log(myArray);
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
