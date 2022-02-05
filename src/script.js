bb4edfeebdbccb0ccae1dc899c64dffbee80956f
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:24:01 +0530

    switch

--- src/script.js
+++ src/script.js
@@ -574,3 +574,12 @@ function sequentialSizes(val) {
   }
   
   chainToSwitch(7);
+  function isLess(a, b) {
+    // Only change code below this line
+    
+      return (a < b) ;
+    
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
