e0ebc8472b0b0daee4f3cfead260b4c998e3651c
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:36:13 +0530

    access objects

--- src/script.js
+++ src/script.js
@@ -621,3 +621,13 @@ const myDog = {
     friends:["a","4","4"]
     // Only change code above this line
   }; 
+  // Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line 
