dccfc337383dbec36552b3e0a02be844f64807e8
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 12:49:59 +0530

    calling a function

--- src/script.js
+++ src/script.js
@@ -181,3 +181,8 @@ function reusableFunction(){
     console.log("Hi World");
   }
   reusableFunction();
+  function functionWithArgs(p1,p2) {
+    console.log(p1+p2);
+    }
+    functionWithArgs(1,2);
+    functionWithArgs(7,9);
