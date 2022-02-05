7f5dc9ed93b76376157ecc167a763491b5a4a8f2
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 12:34:25 +0530

    using pop

--- src/script.js
+++ src/script.js
@@ -159,3 +159,9 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push(["dog" , 3]);
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+ const removedFromMyArray=myArray.pop();
+console.log(myArray);
