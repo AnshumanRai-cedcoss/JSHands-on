63e8d12ef8dcf8df85d947cbfab1e0e2a304c75e
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:04:53 +0530

    switch

--- src/script.js
+++ src/script.js
@@ -495,3 +495,21 @@ return "Go Home!"
 }
 
 golfScore(5, 4);
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+    case 1:
+    answer = "alpha";
+    break;
+     case 2:
+    answer = "beta";
+    break;
+     case 3:
+    answer = "gamma";
+    break;
+     case 4:
+    answer = "delta";
+    break;
+  }
+}  
