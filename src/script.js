814185ab6234fbd4a92372a8d6fd21933aceda8e
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:34:54 +0530

    build objects

--- src/script.js
+++ src/script.js
@@ -613,3 +613,11 @@ function cc(card) {
     return `${count} Hold`
   }
 }
+const myDog = {
+    // Only change code below this line
+    name:"bull dog",
+    legs:2,
+    tails:5,
+    friends:["a","4","4"]
+    // Only change code above this line
+  }; 
