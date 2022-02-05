0d10b60b314de4c3cad354301c7abe862a6ed424
AnshumanRai-cedcoss <anshumanrai@cedcoss.com>
2022-02-05 14:58:54 +0530

    Nested objects

--- src/script.js
+++ src/script.js
@@ -734,5 +734,18 @@ function phoneticLookup(val) {
       ]
     }
   ];
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+      },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"]; 
 
   
